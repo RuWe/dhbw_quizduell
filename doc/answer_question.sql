69236c22168359b7dc0b1c46d50e91bdce4dcd2f
RuWe <Ruthweber95@web.de>
2017-04-13 15:50:09 +0200

    Added insert statement question_answer

---
+++ doc/answer_question.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM quizduell.question_answer
+LIMIT 0, 2000
+
+-- Date: 2017-04-13 15:49
+*/
+INSERT INTO `question_answer` (`Question_id`,`answers_id`) VALUES (0,0);
+INSERT INTO `question_answer` (`Question_id`,`answers_id`) VALUES (0,1);
+INSERT INTO `question_answer` (`Question_id`,`answers_id`) VALUES (0,2);
+INSERT INTO `question_answer` (`Question_id`,`answers_id`) VALUES (0,3);
