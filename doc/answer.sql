39829f2b276882d0ec35c7d91521bf48abd60dd3
RuWe <Ruthweber95@web.de>
2017-04-13 15:35:46 +0200

    Added insert statement
    1 question and 4 answers

---
+++ doc/answer.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM quizduell.answer
+LIMIT 0, 2000
+
+-- Date: 2017-04-13 15:33
+*/
+INSERT INTO `answer` (`id`,`answer`) VALUES (0,'210 x 297 mm');
+INSERT INTO `answer` (`id`,`answer`) VALUES (1,'2 x 3 cm');
+INSERT INTO `answer` (`id`,`answer`) VALUES (2,'0.2 x 0.5 m');
+INSERT INTO `answer` (`id`,`answer`) VALUES (3,'700 x 800 mm');
---
+++ doc/question.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM quizduell.question
+LIMIT 0, 2000
+
+-- Date: 2017-04-13 15:33
+*/
+INSERT INTO `question` (`id`,`question`,`correctAnswer_id`) VALUES (0,'Was sind die exakten Maße eines DIN-A4 Blattes',0);
