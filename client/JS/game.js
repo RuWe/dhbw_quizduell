33e0fbde5159dab6ee9236db7335dc24b7591a23
RuWe <Ruthweber95@web.de>
2017-04-23 22:42:11 +0200

    Changed Client

--- client/JS/game.js
+++ client/JS/game.js
@@ -1,8 +1,30 @@
 //testdata = {"one": "Singular sensation","two": "Beady little eyes", "three": "Little birds pitch by my doorstep"}
 var testdata = '{"list":[{"id":8,"player1":{"id":2,"playerName":"Klaus","score":5},"player2":{"id":1,"playerName":"Peter","score":2},"rounds":[{"id":9,"questions":[],"answer":[]},{"id":10,"questions":[],"answer":[]},{"id":11,"questions":[],"answer":[]},{"id":12,"questions":[],"answer":[]},{"id":13,"questions":[],"answer":[]},{"id":14,"questions":[],"answer":[]}]},{"id":26,"player1":{"id":2,"playerName":"Klaus","score":5},"player2":{"id":1,"playerName":"Peter","score":2},"rounds":[{"id":27,"questions":[{"id":1,"question":"Wann wurde der Automobilkonzern Volkswagen gegründet","answers":[{"id":8,"answer":"1922"},{"id":9,"answer":"1937"},{"id":10,"answer":"1950"},{"id":11,"answer":"1995"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":2,"question":"Welches Institut entwickelte das MP3-Format im Jahr 1982","answers":[{"id":4,"answer":"Frauenhofer-Institut"},{"id":5,"answer":"Max-Planck-Institut"},{"id":6,"answer":"Goethe-Institut"},{"id":7,"answer":"MIT"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":2,"question":"Welches Institut entwickelte das MP3-Format im Jahr 1982","answers":[{"id":4,"answer":"Frauenhofer-Institut"},{"id":5,"answer":"Max-Planck-Institut"},{"id":6,"answer":"Goethe-Institut"},{"id":7,"answer":"MIT"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}}],"answer":[]},{"id":28,"questions":[{"id":1,"question":"Wann wurde der Automobilkonzern Volkswagen gegründet","answers":[{"id":8,"answer":"1922"},{"id":9,"answer":"1937"},{"id":10,"answer":"1950"},{"id":11,"answer":"1995"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":2,"question":"Welches Institut entwickelte das MP3-Format im Jahr 1982","answers":[{"id":4,"answer":"Frauenhofer-Institut"},{"id":5,"answer":"Max-Planck-Institut"},{"id":6,"answer":"Goethe-Institut"},{"id":7,"answer":"MIT"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":2,"question":"Welches Institut entwickelte das MP3-Format im Jahr 1982","answers":[{"id":4,"answer":"Frauenhofer-Institut"},{"id":5,"answer":"Max-Planck-Institut"},{"id":6,"answer":"Goethe-Institut"},{"id":7,"answer":"MIT"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}}],"answer":[]},{"id":29,"questions":[{"id":2,"question":"Welches Institut entwickelte das MP3-Format im Jahr 1982","answers":[{"id":4,"answer":"Frauenhofer-Institut"},{"id":5,"answer":"Max-Planck-Institut"},{"id":6,"answer":"Goethe-Institut"},{"id":7,"answer":"MIT"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":2,"question":"Welches Institut entwickelte das MP3-Format im Jahr 1982","answers":[{"id":4,"answer":"Frauenhofer-Institut"},{"id":5,"answer":"Max-Planck-Institut"},{"id":6,"answer":"Goethe-Institut"},{"id":7,"answer":"MIT"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":2,"question":"Welches Institut entwickelte das MP3-Format im Jahr 1982","answers":[{"id":4,"answer":"Frauenhofer-Institut"},{"id":5,"answer":"Max-Planck-Institut"},{"id":6,"answer":"Goethe-Institut"},{"id":7,"answer":"MIT"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}}],"answer":[]},{"id":30,"questions":[{"id":0,"question":"Was sind die exakten Maße eines DIN-A4 Blattes","answers":[{"id":0,"answer":"210 x 297 mm"},{"id":1,"answer":"2 x 3 cm"},{"id":2,"answer":"0.2 x 0.5 m"},{"id":3,"answer":"700 x 800 mm"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":1,"question":"Wann wurde der Automobilkonzern Volkswagen gegründet","answers":[{"id":8,"answer":"1922"},{"id":9,"answer":"1937"},{"id":10,"answer":"1950"},{"id":11,"answer":"1995"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":2,"question":"Welches Institut entwickelte das MP3-Format im Jahr 1982","answers":[{"id":4,"answer":"Frauenhofer-Institut"},{"id":5,"answer":"Max-Planck-Institut"},{"id":6,"answer":"Goethe-Institut"},{"id":7,"answer":"MIT"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}}],"answer":[]},{"id":31,"questions":[{"id":0,"question":"Was sind die exakten Maße eines DIN-A4 Blattes","answers":[{"id":0,"answer":"210 x 297 mm"},{"id":1,"answer":"2 x 3 cm"},{"id":2,"answer":"0.2 x 0.5 m"},{"id":3,"answer":"700 x 800 mm"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":1,"question":"Wann wurde der Automobilkonzern Volkswagen gegründet","answers":[{"id":8,"answer":"1922"},{"id":9,"answer":"1937"},{"id":10,"answer":"1950"},{"id":11,"answer":"1995"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":1,"question":"Wann wurde der Automobilkonzern Volkswagen gegründet","answers":[{"id":8,"answer":"1922"},{"id":9,"answer":"1937"},{"id":10,"answer":"1950"},{"id":11,"answer":"1995"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}}],"answer":[]},{"id":32,"questions":[{"id":1,"question":"Wann wurde der Automobilkonzern Volkswagen gegründet","answers":[{"id":8,"answer":"1922"},{"id":9,"answer":"1937"},{"id":10,"answer":"1950"},{"id":11,"answer":"1995"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":1,"question":"Wann wurde der Automobilkonzern Volkswagen gegründet","answers":[{"id":8,"answer":"1922"},{"id":9,"answer":"1937"},{"id":10,"answer":"1950"},{"id":11,"answer":"1995"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}},{"id":2,"question":"Welches Institut entwickelte das MP3-Format im Jahr 1982","answers":[{"id":4,"answer":"Frauenhofer-Institut"},{"id":5,"answer":"Max-Planck-Institut"},{"id":6,"answer":"Goethe-Institut"},{"id":7,"answer":"MIT"}],"correctAnswer":{"id":0,"answer":"210 x 297 mm"}}],"answer":[]}]}]}';
 function loadDuell() {
+    $.ajax({
+        dataType: "json",
+        url: "test.json",
+        success: function(data) {
+            $.each(data, function(i, obj) {
+                //alert(i + " " + obj)
+                //$('#player'+id+'Question').append($('<label for="'+i+'">'+obj+'</label>'));
+                $('#duells').append($('<button>' + obj.id + '</button>'));
+            });
+            //$('#player'+id+'Question').append($('<div id="test"+id>' + data +'</div>'));
+            //$('#player'+id+'Answer').append($('<button id="testA"+id>' + data +'</button>'));
+
+        },
+        error: function (data) {
+            $.each(data, function(i, obj) {
+                //(obj);
+                alert(i + " " + obj);
+                //$('#player'+id+'Question').append($('<label for="'+i+'">'+obj+'</label>'));
+                //$('#player'+id+'Answer').append($('<input id="'+i+'" value="'+data.value+'" type="text"/><br>'));
+            });
+        }
+    });
     //alert(id)
-    let data = JSON.parse(testdata);
+    /*let data = JSON.parse(testdata);
     let output = "";
     for (let i = 0; i <= data.list.length -1; i++){
         for (key in data.list[i]) {
@@ -11,22 +33,33 @@ function loadDuell() {
             }
         }
     }
-    alert(output);
+    alert(output);*/
 }
 
 function loadQuestion(id){
     $.ajax({
         dataType: "json",
         url: "test.json",
-        mimeType: "application/json",
         success: function(data) {
             $.each(data, function(i, obj) {
-                $('#player'+id+'Question').append($('<label for="'+i+'">'+obj.title+'</label>'));
-                $('#player'+id+'Answer').append($('<input id="'+i+'" value="'+obj.value+'" type="text"/><br>'));
+                $.each(obj, function (j, o) {
+                    $('#player'+id+'Question').append($('<button>' + o.id + '</button>'));
+                })
+
+                //$('#player'+id+'Question').append($('<label for="'+i+'">'+obj+'</label>'));
+                //$('#player'+id+'Answer').append($('<input id="'+i+'" value="'+obj+'" type="text"/><br>'));
             });
             //$('#player'+id+'Question').append($('<div id="test"+id>' + data +'</div>'));
             //$('#player'+id+'Answer').append($('<button id="testA"+id>' + data +'</button>'));
 
+        },
+        error: function (data) {
+            $.each(data, function(i, obj) {
+                alert(obj);
+                //alert(i + " " + obj);
+                //$('#player'+id+'Question').append($('<label for="'+i+'">'+obj+'</label>'));
+                //$('#player'+id+'Answer').append($('<input id="'+i+'" value="'+data.value+'" type="text"/><br>'));
+            });
         }
     });
 }
--- src/main/java/de/dhbw/quizduell/REST/RestServer.java
+++ src/main/java/de/dhbw/quizduell/REST/RestServer.java
@@ -2,8 +2,11 @@ package de.dhbw.quizduell.REST;
 
 import org.restlet.Component;
 import org.restlet.data.Protocol;
+import org.restlet.service.CorsService;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
 
 /**
  * Created by Ruth Weber on 10.04.2017.
@@ -11,7 +14,12 @@ import java.io.IOException;
 public class RestServer {
 
     public static void main(String[] args) throws IOException, Exception {
+        CorsService corsService = new CorsService();
+        corsService.setAllowedOrigins(new HashSet(Collections.singletonList("*")));
+        corsService.setAllowedCredentials(true);
+
         Component component = new Component();
+        component.getServices().add(corsService);
         component.getServers().add(Protocol.HTTP, 8085);
         component.getDefaultHost().attach("/rest", new RestApp());
         component.start();
